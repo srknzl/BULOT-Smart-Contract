23e357253da7ad94d82e7ae34a8585634187c210
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-11-16 13:05:05 +0300

    update baho's bulot

--- bulot-baho.sol
+++ bulot-baho.sol
@@ -1,37 +1,48 @@
 pragma solidity >=0.4.21;
 
-import "./EIP20Interface.sol";
 import "./EIP20.sol";
+/*
+0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c
+0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
+0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB
+
+Bulot contract:
+0x375EaE23b65fEB1833072328647902f1FE9afA61
+//*/
 
 contract BULOTContract {
-    enum Stages { submission, reveal }
-    
-    Stages current;
-    uint nextStageTimestamp;
-    mapping(address=>uint256) players;
+
+    bool isSubmission;
+    uint public nextStageTimestamp;
+    mapping(address=>bytes32) players;
     mapping(address=>bool) cheaters;
     uint numPlayers;
     uint numCheaters;
+    address[] revealedPlayers;
+    uint numRevealedPlayers;
     
-    EIP20Interface tokenNetwork;
+    EIP20 network;
     
-    function BULOTContract() public {
-        current = Stages.submission;
-        nextStageTimestamp = now + 604800;  // adds 1 week
-        tokenNetwork = EIP20Interface(this);
+    function BULOTContract(address _network) public {
+        network = EIP20(_network);
+        isSubmission = true;
+        nextStageTimestamp = now + 604800;  // adds 1 week to the current date
         numPlayers = 0;
-        cheaters = 0;
+        numCheaters = 0;
     }
     
-    function () public {    // prevent random payments
+    function () public {    // prevent random payments in fallback function
         revert();
     }
-     
-    modifier donePayment(uint _amount) {
-        require(1==1);  // TODO: do something making sense
-        _;
+
+    function purchaseTicket(bytes32 randomHashed) public returns(bool) {
+        require(network.transferFrom(msg.sender, address(this), 10));
+        players[msg.sender] = randomHashed;
+        numPlayers++;
+        return true;
     }
     
-    function purchaseTicket(bytes32 _random) public donePayment(10) {
+    function hashRandomNumber(uint randomNum) public view returns (uint256 hashed) {
+        return uint256(keccak256(randomNum, msg.sender));
     }
 }
