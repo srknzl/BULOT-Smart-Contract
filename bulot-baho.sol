f4ba174416313adb053ea0c4c169e31b9bd0313f
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-11-17 12:27:33 +0300

    add winner calculation

--- bulot-baho.sol
+++ bulot-baho.sol
@@ -15,12 +15,9 @@ contract BULOTContract {
     bool isSubmission;
     uint public nextStageTimestamp;
     mapping(address=>bytes32) players;
-    mapping(address=>bool) cheaters;
-    uint numPlayers;
-    uint numCheaters;
     address[] revealedPlayers;
-    uint numRevealedPlayers;    // TODO: redundant? revealedPlayers.length?
     mapping(address => uint) winners;
+    uint randomAccumulator;
     
     EIP20 network;
     
@@ -29,8 +26,7 @@ contract BULOTContract {
         // TODO: make it not create when _network isn't given
         isSubmission = true;
         nextStageTimestamp = now + STAGEDURATION;   // adds 1 week to the current date
-        numPlayers = 0;
-        numCheaters = 0;
+        randomAccumulator = 0;
     }
     
     function () public {    // prevent random payments in fallback function
@@ -47,9 +43,7 @@ contract BULOTContract {
                 // TODO: when reveal is finished
                 findWinners();
                 // TODO: delete players; not allowed
-                numPlayers = 0;
                 delete revealedPlayers;
-                delete numRevealedPlayers;
             }
             isSubmission = !isSubmission;
             nextStageTimestamp += STAGEDURATION;
@@ -64,14 +58,12 @@ contract BULOTContract {
     
     modifier reveal() {
         require(!isSubmission);
-        require(players[msg.sender] > 0);   // TODO: needed?
         _;
     }
 
     function purchaseTicket(bytes32 randomHashed) public stageUpToDate submission returns(bool) {
         require(network.transferFrom(msg.sender, address(this), 10));
         players[msg.sender] = randomHashed;
-        numPlayers++;
         return true;
     }
     
@@ -80,8 +72,8 @@ contract BULOTContract {
         if(players[msg.sender] == hashed) {
             // TODO: when the player didn't lie
             revealedPlayers.push(msg.sender);
-            numRevealedPlayers++;
             delete players[msg.sender];
+            randomAccumulator = randomAccumulator ^ randomNum;
             return true;
         } else {
             // TODO when the player lied
@@ -142,13 +134,16 @@ contract BULOTContract {
         uint indexRange = logarithm2(M);        // log2(M)
         
         uint P;
+        address winner;
         for(uint i=1; i <= indexRange; i++) {
             // calculate P_i 
             M = integerDivision(M, 2);
             P = M;
             M = integerDivision(M, 2);
             P += M % 2;
-            // TODO: assign the prize to the winner
+            // TODO: assign the prize to the winner. done, require checks
+            winner = revealedPlayers[uint(keccak256(randomAccumulator)) % revealedPlayers.length];
+            winners[winner] += P;
         }
     }
     
