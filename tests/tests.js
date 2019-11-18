5c7bd974f3df83837ff3b43041e21077ee397aed
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-11-18 22:05:19 +0300

    Revert "Add initial tests folder not sure how to test"
    This reverts commit 9423844639dda2a581996107434e1cd4aa48d013.

--- tests/tests.js
+++
@@ -1,5 +0,0 @@
-const addresscontract = "0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C";
-const abicontract = require("./abi");
-hellocontract = web3.eth.contract(abicontract).at(addresscontract);
-eth.defaultAccount= eth.accounts[0];
-personal.unlockAccount(eth.accounts[0],"password",100) ;
